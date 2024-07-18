cf8e40d703d18640016b2dd4e7d09fd0658de4ef
Gulsah <gulsharslan@gmail.com>
2024-07-18 14:33:30 +0300

    added mediator

---
+++ Services/Order/Core/TakeAway.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeAway.Application.Features.CQRS.Queries.OrderDetailQueries;
+using TakeAway.Application.Features.CQRS.Results.OrderDetailResults;
+using TakeAway.Application.Interfaces;
+using TakeAway.Domain.Entities;
+
+namespace TakeAway.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class GetOrderDetailByIdQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public GetOrderDetailByIdQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetOrderDetailByIdQueryResult
+            {
+                OrderDetailId = values.OrderDetailId,
+                Amount = values.Amount,
+                ProductName= values.ProductName,
+                ProductId= values.ProductId,
+                TotalPrice= values.TotalPrice,
+                Price= values.Price,
+                OrderingId=values.OrderingId,
+            };
+        }
+}
