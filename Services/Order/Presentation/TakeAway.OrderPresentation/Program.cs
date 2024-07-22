02abb0e463651efd44a4f94c8e9b616d39a6627c
Gulsah <gulsharslan@gmail.com>
2024-07-22 22:52:55 +0300

    ..

--- Services/Order/Presentation/TakeAway.OrderPresentation/Program.cs
+++ Services/Order/Presentation/TakeAway.OrderPresentation/Program.cs
@@ -1,6 +1,41 @@
+using TakeAway.Application.Features.CQRS.Handlers.AddressHandlers;
+using TakeAway.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using TakeAway.Application.Interfaces;
+using TakeAway.Application.Services;
+using TakeAway.Persistance.Context;
+using TakeAway.Persistance.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<OrderContext>();
+
+#region CQRS OrderDetail
+
+builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
+builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+
+#endregion 
+
+#region CQRS Address
+
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+builder.Services.AddScoped<RemoveAddressCommandHandler>();
+
+#endregion
+
+#region Repository Mediator
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
