e3ebd7c3a01ffbcc5c8a7ecfb0bc2c48b707d9f5
Gulsah <gulsharslan@gmail.com>
2024-07-26 14:01:01 +0300

    ..

---
+++ Services/Basket/TakeAway.Baskett/Services/BasketService.cs
@@ -0,0 +1,22 @@
+﻿using TakeAway.Baskett.Dtos;
+
+namespace TakeAway.Baskett.Services
+{
+    public class BasketService : IBasketService
+    {
+        public Task DeleteBasket(string UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BasketTotalDto> GetBasket(string UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveBasket(BasketTotalDto basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Basket/TakeAway.Baskett/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using TakeAway.Baskett.Dtos;
+
+namespace TakeAway.Baskett.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketTotalDto> GetBasket(string UserId);
+        Task SaveBasket (BasketTotalDto basket);
+        Task DeleteBasket (string UserId);
+    }
+}
