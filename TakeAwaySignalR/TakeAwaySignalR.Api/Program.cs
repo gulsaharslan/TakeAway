e002125bad1daf1d0619180c9cdd541434c4255d
Gulsah <gulsharslan@gmail.com>
2024-08-13 14:55:08 +0300

    ..

--- TakeAwaySignalR/TakeAwaySignalR.Api/Program.cs
+++ TakeAwaySignalR/TakeAwaySignalR.Api/Program.cs
@@ -1,4 +1,5 @@
 using TakeAwaySignalR.Api.DAL;
+using TakeAwaySignalR.Api.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +18,7 @@ builder.Services.AddCors(opt =>
     });
 });
 
-
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -38,5 +39,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
